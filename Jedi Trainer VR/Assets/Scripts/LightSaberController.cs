c603984528fbf528f8f32ffc4cd03510963d9d6c
Ethan Legum <71243839+elegs99@users.noreply.github.com>
2024-01-27 18:20:26 -0500

    Made the lightsaber way cooler
    Added effects when colliding with objects
    Particle for sparks
    green glow at intersection point
    line render to leave a hot path

--- Jedi Trainer VR/Assets/Scripts/LightSaberController.cs
+++ Jedi Trainer VR/Assets/Scripts/LightSaberController.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LightSaberController : MonoBehaviour
 {
     public GameObject lightBlade;
     public CapsuleCollider lightsaberCollider;
-
+    public GameObject saberHitEffect;
+    public Transform saberBase;
+    bool isExtended = false;
+    bool isEffectSpawned = false;
+    GameObject hitEffect;
+    void Update() {
+        if (isExtended) {
+            if (isEffectSpawned) {
+                if (Physics.Raycast(saberBase.position, -saberBase.transform.up, out RaycastHit hit, .9f)) {
+                    Debug.Log("moving");
+                    hitEffect.transform.position = hit.point;
+                } else {
+                    Debug.Log("Destroying");
+                    Destroy(hitEffect);
+                    isEffectSpawned = false;
+                }
+            } else {
+                if (Physics.Raycast(saberBase.position, -saberBase.transform.up, out RaycastHit hit, .9f)) {
+                    Debug.Log("spawning");
+                    hitEffect = Instantiate(saberHitEffect, hit.point, hit.transform.rotation);
+                    isEffectSpawned = true;
+                }
+            }
+        } else {
+            Debug.Log("Destroying");
+            Destroy(hitEffect);
+            isEffectSpawned = false;
+        }
+    }
     public void ExtendBlade() {
+        isExtended = true;
         lightBlade.SetActive(true);
         lightsaberCollider.height = 24;
         lightsaberCollider.center = new Vector3(0,0,8.6f);
     }
     public void RetractBlade() {
+        isExtended = false;
         lightBlade.SetActive(false);
         lightsaberCollider.height = 8;
         lightsaberCollider.center = new Vector3(0,0,0.5f);
